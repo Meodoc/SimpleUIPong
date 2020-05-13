4adaa8154cdb16969ed89d9a93b522a9ed30cb6e
Meodoc <games.luki@gmail.com>
2020-05-13 20:26:07 +0200

    Player and Enemy Rectangle are created dynamically

--- SimpleUIPong/App.xaml.cs
+++ SimpleUIPong/App.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace SimpleUIPong
+﻿using System;
+using System.Windows;
+
+namespace SimpleUIPong
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App : Application
     {
+        
     }
 }
--- SimpleUIPong/MainWindow.xaml.cs
+++ SimpleUIPong/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿namespace SimpleUIPong
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SimpleUIPong
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +15,60 @@
         public MainWindow()
         {
             InitializeComponent();
+            CreatePlayerRect();
+            CreateEnemyRect();
+
+            
+        }
+
+        public void CreatePlayerRect()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Height = 100;
+            rectangle.Width = 20;
+
+            // Create brushes
+            SolidColorBrush blueBrush = new SolidColorBrush();
+            blueBrush.Color = Colors.Blue;
+            SolidColorBrush blackBrush = new SolidColorBrush();
+            blackBrush.Color = Colors.Black;
+
+            // Set stroke
+            rectangle.StrokeThickness = 2;
+            rectangle.Stroke = blackBrush;
+
+            // Fill rectangle with blue color  
+            rectangle.Fill = blueBrush;
+
+            // Add Rectangle to the Grid.  
+            RootCanvas.Children.Add(rectangle);
+            Canvas.SetTop(rectangle, 50);
+            Canvas.SetLeft(rectangle, 10);
+        }
+
+        public void CreateEnemyRect()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Height = 100;
+            rectangle.Width = 20;
+
+            // Create brushes
+            SolidColorBrush redBrush = new SolidColorBrush();
+            redBrush.Color = Colors.Red;
+            SolidColorBrush blackBrush = new SolidColorBrush();
+            blackBrush.Color = Colors.Black;
+
+            // Set stroke
+            rectangle.StrokeThickness = 2;
+            rectangle.Stroke = blackBrush;
+
+            // Fill rectangle with blue color  
+            rectangle.Fill = redBrush;
+
+            // Add Rectangle to the Grid.  
+            RootCanvas.Children.Add(rectangle);
+            Canvas.SetTop(rectangle, 50);
+            Canvas.SetRight(rectangle, 10);
         }
     }
 }
